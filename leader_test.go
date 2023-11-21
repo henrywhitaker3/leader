e44e7e1c7876a58261869a549d23f53f8c04fb15
Henry Whitaker <henrywhitaker3@outlook.com>
2023-11-21 15:12:15 +0000

    Added test for ousting

--- leader_test.go
+++ leader_test.go
@@ -65,8 +65,61 @@ func TestItRenewsItsOwnLock(t *testing.T) {
 		Instance: "1",
 		Expires:  now().Add(time.Second * 15),
 	}, 0).SetVal("OK")
+
+	onElectionCalled := false
+	onRenewalCalled := false
+	onOustingCalled := false
+	onErrorCalled := false
+
+	leader := &LeaderManager{
+		Name:     "bongo",
+		Instance: "1",
+		Locker:   NewRedisLocker(client),
+
+		OnElection: func(instance string) {
+			onElectionCalled = true
+		},
+		OnRenewal: func(instance string) {
+			onRenewalCalled = true
+		},
+		OnOusting: func(instance string) {
+			onOustingCalled = true
+		},
+		OnError: func(instance string, err error) {
+			fmt.Println(err)
+			onErrorCalled = true
+		},
+	}
+
+	leader.run(context.Background())
+
+	assert.True(t, onElectionCalled)
+	assert.False(t, onRenewalCalled)
+	assert.False(t, onOustingCalled)
+	assert.False(t, onErrorCalled)
+
+	onElectionCalled = false
 	mock.ExpectGet("bongo-leader").SetVal(`{"Instance":"1","Expires":"2023-11-21T15:04:00Z"}`)
 	mock.ExpectGet("bongo-leader").SetVal(`{"Instance":"1","Expires":"2023-11-21T15:04:00Z"}`)
+	mock.ExpectSet("bongo-leader", &Lock{
+		Instance: "1",
+		Expires:  now().Add(time.Second * 15),
+	}, 0).SetVal("OK")
+	leader.run(context.Background())
+
+	assert.False(t, onElectionCalled)
+	assert.True(t, onRenewalCalled)
+	assert.False(t, onOustingCalled)
+	assert.False(t, onErrorCalled)
+}
+
+func TestItCallsOnOustedWhenAnotherInstanceTakesOverTheLock(t *testing.T) {
+	now = func() time.Time {
+		fakeTime, _ := time.Parse(time.RFC3339, "2023-11-21T15:04:05Z")
+		return fakeTime
+	}
+	client, mock := redismock.NewClientMock()
+	mock.ExpectGet("bongo-leader").RedisNil()
 	mock.ExpectSet("bongo-leader", &Lock{
 		Instance: "1",
 		Expires:  now().Add(time.Second * 15),
@@ -105,10 +158,13 @@ func TestItRenewsItsOwnLock(t *testing.T) {
 	assert.False(t, onErrorCalled)
 
 	onElectionCalled = false
+	mock.ExpectGet("bongo-leader").SetVal(`{"Instance":"2","Expires":"2023-11-21T15:20:00Z"}`)
+	mock.ExpectGet("bongo-leader").SetVal(`{"Instance":"2","Expires":"2023-11-21T15:20:00Z"}`)
+
 	leader.run(context.Background())
 
 	assert.False(t, onElectionCalled)
-	assert.True(t, onRenewalCalled)
-	assert.False(t, onOustingCalled)
+	assert.False(t, onRenewalCalled)
+	assert.True(t, onOustingCalled)
 	assert.False(t, onErrorCalled)
 }
